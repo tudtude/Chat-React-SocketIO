ab08642b0e4238b2fc83cd0c792f2c79fdd559a3
tudtude <tudtude@gmail.com>
2019-03-19 20:42:42 +0700

    add build

---
+++ build/precache-manifest.80ef1b6acb37cb929832cde8ee51c719.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "760c68b09f992f838de0",
+    "url": "/static/js/main.956bee51.chunk.js"
+  },
+  {
+    "revision": "fdeb5c10d27bc31aff10",
+    "url": "/static/js/2.a8f4c8e1.chunk.js"
+  },
+  {
+    "revision": "760c68b09f992f838de0",
+    "url": "/static/css/main.05b4cfd1.chunk.css"
+  },
+  {
+    "revision": "fdeb5c10d27bc31aff10",
+    "url": "/static/css/2.1f92ef3f.chunk.css"
+  },
+  {
+    "revision": "6a52bb284bde9016f02da246a8c4bf5c",
+    "url": "/index.html"
+  }
+];
